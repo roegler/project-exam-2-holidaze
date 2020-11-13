5e841afcb459bbbd207b22a76a2fef93d45db61c
Marie Røgler <mroegler@gmail.com>
2020-11-13 11:37:38 +0100

    adding contactform component and submitbtn

---
+++ src/components/SubmitBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SubmitBtn() {
+    return(
+        <button type="button" class="btn btn-primary">SUBMIT</button>
+    );
+}
+
+export default SubmitBtn;
+
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactForm from '../components/ContactForm';
 
 function Contact () {
     return (
     <div>
-        Contact
+        <ContactForm />
     </div>
     );
 }
